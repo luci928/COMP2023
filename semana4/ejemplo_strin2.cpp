aebe070cf02d72b9db904b8974577672ad118e95
luci928 <107441282+luci928@users.noreply.github.com>
2023-04-05 08:31:22 -0500

    Add files via upload

---
+++ semana4/ejemplo_strin2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char cadena;
+    cout<<"ingrese una cadena de numeros: "<<endl;
+    cin>>cadena;
+
+    int contador=0;
+
+    for(int i = 0;i<cadena.length();i++){
+        if(caracte == cad.at(i)){
+            contador++;
+        }
+    }
+    
+    cout<<"el caracter"<<caracter <<"se repite "<< contador << " veces "<<endl;
+    
+    return 0 ;
+}
