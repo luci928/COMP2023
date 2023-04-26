8a5a7c7cf36164c8c94c5961e457350031170e6a
luci928 <107441282+luci928@users.noreply.github.com>
2023-04-26 09:02:09 -0500

    Actualizar Arr_Pal_It_Prof.cpp

---
+++ semana7/Arr_Pal_It_Prof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+bool esPalIter(const int arr[],const int tam[]{
+   int ini=0;
+   int fin = tam-1;
+   while(ini<fin){
+      if(arr[ini] != arr[fin]){
+         return true;
+
+}
+
+
+}
